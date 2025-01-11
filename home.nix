c56a78752ae11006ba03427a5c556ec2d961e7bc
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-11 13:47:06 +0100

    Add Ghostty config

--- home.nix
+++ home.nix
@@ -8,4 +8,27 @@
   xdg = {
     enable = true;
   };
+
+  xdg.configFile = {
+    "ghostty/config".text = ''
+      theme = GruvboxDark
+
+      title = " "
+      macos-titlebar-proxy-icon = hidden
+
+      font-family = JetBrainsMono Nerd Font Mono
+      font-size = 16
+      window-inherit-font-size = true
+
+      window-width = 128
+      window-height = 40
+
+      macos-option-as-alt = true
+
+      macos-icon = custom-style
+      macos-icon-ghost-color = #fbf1c7
+      macos-icon-screen-color = #282828
+      macos-icon-frame = aluminum
+    '';
+  };
 }
