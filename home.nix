4345a8ed47da115b5e6bef518c1ec9fefa0e5c1f
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-14 18:48:43 +0100

    Configure GitHub auth & signing SSH keys

--- home.nix
+++ home.nix
@@ -31,4 +31,43 @@
       macos-icon-frame = aluminum
     '';
   };
+
+  age.identityPaths = [
+    "${config.home.homeDirectory}/.ssh/nix_system_secrets_decryption_ed25519"
+  ];
+
+  age.secrets.jessevdp_github_auth = {
+    file = "${config.home.homeDirectory}/.config/nix-system/secrets/ssh/jessevdp_github_auth_ed25519.age";
+    path = "${config.home.homeDirectory}/.ssh/jessevdp_github_auth_ed25519";
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        identityFile = "~/.ssh/jessevdp_github_auth_ed25519";
+      };
+    };
+    extraConfig = ''
+      UseKeychain yes
+      AddKeysToAgent yes
+    '';
+  };
+
+  age.secrets.jessevdp_github_signing = {
+    file = "${config.home.homeDirectory}/.config/nix-system/secrets/ssh/jessevdp_github_signing_ed25519.age";
+    path = "${config.home.homeDirectory}/.ssh/jessevdp_github_signing_ed25519";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Jesse van der Pluijm";
+    userEmail = "jessevdp@hey.com";
+
+    extraConfig = {
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/jessevdp_github_signing_ed25519";
+      commit.gpgsign = true;
+    };
+  };
 }
