fc773ea851a3f85f148a2b6b3e09be332114ac46
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-15 10:52:34 +0100

    Move nix-darwin config to its own file

---
+++ darwin.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      cleanup = "zap";
+    };
+    casks = [
+      "ghostty"
+    ];
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+    swapLeftCtrlAndFn = true;
+  };
+
+  system.defaults.dock = {
+    autohide = true;
+    persistent-apps = [
+      "/System/Cryptexes/App/System/Applications/Safari.app" # "/Applications/Safari.app"
+      "/Applications/Ghostty.app"
+    ];
+    persistent-others = [
+      "${config.users.users."jessevanderpluijm".home}/Downloads"
+    ];
+    show-recents = false;
+  };
+
+  system.defaults.dock.mru-spaces = false;
+
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    FXPreferredViewStyle = "clmv";
+    _FXSortFoldersFirst = true;
+    _FXSortFoldersFirstOnDesktop = true;
+  };
+}
--- flake.nix
+++ flake.nix
@@ -30,82 +30,29 @@
     home-manager,
     agenix,
     ...
-  }: let
-    darwin-configuration = {
-      config,
-      pkgs,
-      ...
-    }: {
-      environment.systemPackages = with pkgs; [
-        git
-        vim
-      ];
-
-      homebrew = {
-        enable = true;
-        onActivation = {
-          cleanup = "zap";
-        };
-        casks = [
-          "ghostty"
-        ];
-      };
-
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-
-      system.keyboard = {
-        enableKeyMapping = true;
-        remapCapsLockToControl = true;
-        swapLeftCtrlAndFn = true;
-      };
-
-      system.defaults.dock = {
-        autohide = true;
-        persistent-apps = [
-          "/System/Cryptexes/App/System/Applications/Safari.app" # "/Applications/Safari.app"
-          "/Applications/Ghostty.app"
-        ];
-        persistent-others = [
-          "${config.users.users."jessevanderpluijm".home}/Downloads"
-        ];
-        show-recents = false;
-      };
-
-      system.defaults.dock.mru-spaces = false;
-
-      system.defaults.finder = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        FXPreferredViewStyle = "clmv";
-        _FXSortFoldersFirst = true;
-        _FXSortFoldersFirstOnDesktop = true;
-      };
-
-      users.users."jessevanderpluijm" = {
-        home = "/Users/jessevanderpluijm";
-      };
-
-      # We install Nix using a separate installer so we don't want nix-darwin
-      # to manage it for us. This tells nix-darwin to just use whatever is running.
-      nix.useDaemon = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in {
+  }: {
     darwinConfigurations."LJQPCW4D95" = darwin.lib.darwinSystem {
       modules = [
-        darwin-configuration
+        {
+          users.users."jessevanderpluijm" = {
+            home = "/Users/jessevanderpluijm";
+          };
+
+          # We install Nix using a separate installer so we don't want nix-darwin
+          # to manage it for us. This tells nix-darwin to just use whatever is running.
+          nix.useDaemon = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        }
+        ./darwin.nix
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
