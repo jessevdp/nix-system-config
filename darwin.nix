325cfd3adc105d974914b751f20b2e0b6c53ad50
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-15 10:57:36 +0100

    Disable "change extension" warning in MacOS Finder

--- darwin.nix
+++ darwin.nix
@@ -46,6 +46,7 @@
   system.defaults.finder = {
     AppleShowAllExtensions = true;
     AppleShowAllFiles = true;
+    FXEnableExtensionChangeWarning = false;
     FXPreferredViewStyle = "clmv";
     _FXSortFoldersFirst = true;
     _FXSortFoldersFirstOnDesktop = true;
