7775b1fe39eedb6ce308d995e7f9e7a31e275df2
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-16 08:33:38 +0100

    Use existing path variable to refer to secrets
    So I don't have to edit the path in 2 places

--- home/git.nix
+++ home/git.nix
@@ -5,7 +5,7 @@
   };
 
   programs.ssh.matchBlocks."github.com" = {
-    identityFile = "~/.ssh/jessevdp_github_auth_ed25519";
+    identityFile = config.age.secrets.jessevdp_github_auth.path;
   };
 
   age.secrets.jessevdp_github_signing = {
@@ -20,7 +20,7 @@
 
     extraConfig = {
       gpg.format = "ssh";
-      user.signingkey = "~/.ssh/jessevdp_github_signing_ed25519";
+      user.signingkey = config.age.secrets.jessevdp_github_signing.path;
       commit.gpgsign = true;
     };
   };
