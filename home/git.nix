b29d084c9f9815f078af3301dd9dd6b7562b1cf8
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-16 15:57:56 +0100

    Name git related age secrets more descriptively as SSH keys

--- home/git.nix
+++ home/git.nix
@@ -1,14 +1,14 @@
 {config, ...}: {
-  age.secrets.jessevdp_github_auth = {
+  age.secrets.jessevdp_github_auth_ssh_key = {
     file = "${config.home.homeDirectory}/.config/nix-system/secrets/ssh/jessevdp_github_auth_ed25519.age";
     path = "${config.home.homeDirectory}/.ssh/jessevdp_github_auth_ed25519";
   };
 
   programs.ssh.matchBlocks."github.com" = {
-    identityFile = config.age.secrets.jessevdp_github_auth.path;
+    identityFile = config.age.secrets.jessevdp_github_auth_ssh_key.path;
   };
 
-  age.secrets.jessevdp_github_signing = {
+  age.secrets.jessevdp_github_signing_ssh_key = {
     file = "${config.home.homeDirectory}/.config/nix-system/secrets/ssh/jessevdp_github_signing_ed25519.age";
     path = "${config.home.homeDirectory}/.ssh/jessevdp_github_signing_ed25519";
   };
@@ -20,7 +20,7 @@
 
     extraConfig = {
       gpg.format = "ssh";
-      user.signingkey = config.age.secrets.jessevdp_github_signing.path;
+      user.signingkey = config.age.secrets.jessevdp_github_signing_ssh_key.path;
       commit.gpgsign = true;
     };
   };
