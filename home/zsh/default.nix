1f3c5b657a709ba4647f6c6ffcf9c6cc1095c051
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-17 19:08:00 +0100

    Configure zsh prompt (powerlevel10k)

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,4 +1,8 @@
 {
+  imports = [
+    ./prompt
+  ];
+
   programs.zsh = {
     enable = true;
   };
---
+++ home/zsh/prompt/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  filename = "p10k/p10k.zsh";
+in {
+  xdg.configFile."${filename}".source = ./p10k.zsh;
+
+  programs.zsh = {
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    initExtraFirst =
+      # sh
+      ''
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "${config.xdg.cacheHome}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "${config.xdg.cacheHome}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
+      '';
+
+    initExtra =
+      # sh
+      ''
+        [[ ! -f ${config.xdg.configHome}/${filename} ]] || source ${config.xdg.configHome}/${filename}
+      '';
+  };
+}
