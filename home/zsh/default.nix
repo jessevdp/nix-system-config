b47c91e2c43ef318d76322d855a191309176c990
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-21 10:11:09 +0100

    Configure zsh fzf integration

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./fzf.nix
     ./prompt
     ./vi-mode
   ];
---
+++ home/zsh/fzf.nix
@@ -0,0 +1,56 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    fd
+  ];
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+
+    # For CTRL-T and direct fzf command (files only)
+    defaultCommand = "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
+
+    # For ALT-C (directories only)
+    changeDirWidgetCommand = "fd --type d --strip-cwd-prefix --hidden --follow --exclude .git";
+
+    defaultOptions = [
+      "--layout=reverse"
+      "--border sharp"
+      "--info inline-right"
+    ];
+
+    colors = {
+      "fg" = "white";
+      "fg+" = "#ebdbb2"; # NOTE: non-standard
+      "preview-fg" = "white";
+      "bg" = "black";
+      "bg+" = "#3c3836"; # NOTE: non-standard
+      "preview-bg" = "black";
+      "hl" = "blue";
+      "hl+" = "blue";
+      "info" = "yellow";
+      "border" = "#3c3836"; # NOTE: non-standard
+      "prompt" = "yellow";
+      "pointer" = "red";
+      "marker" = "red";
+      "spinner" = "red";
+      "header" = "blue";
+    };
+  };
+
+  programs.zsh = {
+    plugins = [
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+    ];
+
+    initExtra =
+      # sh
+      ''
+        bindkey -M vicmd / fzf-history-widget
+      '';
+  };
+}
