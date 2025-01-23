11d50056112967cc464bb2284b72c570ee5804bd
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-23 17:22:24 +0100

    Set home-manager backup file extension

--- flake.nix
+++ flake.nix
@@ -49,6 +49,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.users.jessevanderpluijm = {...}: {
             imports = [
               ./home
