fff90a230dfd3afaadecf2702c2197157f1e7950
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-26 09:28:46 +0100

    Setup & configure Zellij

--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,7 @@
     ./lazygit.nix
     ./safari.nix
     ./ssh.nix
+    ./zellij
     ./zsh
 
     ./work-io
---
+++ home/zellij/default.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  home.packages = with pkgs; [
+    zellij
+  ];
+
+  xdg.configFile = {
+    "zellij/config.kdl".source = ./config.kdl;
+    "zellij/layouts".source = ./layouts;
+  };
+
+  programs.zsh.initExtra = lib.mkOrder 200
+    # sh
+    ''
+      # FIXME: the regular zellij zsh completion integration is broken. See:
+      # https://github.com/zellij-org/zellij/issues/1933
+      # So instead of:
+      #   eval "$(${lib.getExe pkgs.zellij} setup --generate-completion zsh)"
+      # We use: (from: https://github.com/zellij-org/zellij/issues/1933#issuecomment-2274464004)
+      . <( ${lib.getExe pkgs.zellij} setup --generate-completion zsh | sed -Ee 's/^(_(zellij) ).*/compdef \1\2/' )
+    '';
+}
