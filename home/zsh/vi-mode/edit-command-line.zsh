fdc920481000e3fb37aa4a0df34a2b97b91a2612
Jesse van der Pluijm <jessevdp@hey.com>
2025-02-04 09:31:20 +0100

    Add additional zsh keymaps to 'edit-command-line' in $EDITOR

--- home/zsh/vi-mode/edit-command-line.zsh
+++ home/zsh/vi-mode/edit-command-line.zsh
@@ -1,4 +1,11 @@
 # Edit current command in $EDITOR
 autoload -Uz edit-command-line
 zle -N edit-command-line
-bindkey '^v' edit-command-line
+
+bindkey -M viins '^v' edit-command-line
+bindkey -M vicmd '^v' edit-command-line
+bindkey -M visual '^v' edit-command-line
+
+# Emulate the vim 'cmdwin'
+bindkey -M vicmd 'q:' edit-command-line # to mirror "edit Ex command-line"
+bindkey -M viins '^f' edit-command-line # to mirror 'cedit'
