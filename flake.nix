76385f270d32396f314c0e78f6c2f3067190c12a
Jesse van der Pluijm <jessevdp@hey.com>
2025-03-10 11:25:43 +0100

    Add mac-app-util to be able to launch apps with Spotlight

--- flake.nix
+++ flake.nix
@@ -11,6 +11,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        systems.follows = "systems";
+      };
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,46 +27,93 @@
 
     agenix = {
       url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.darwin.follows = "darwin";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        darwin.follows = "darwin";
+        home-manager.follows = "home-manager";
+        systems.follows = "systems";
+      };
     };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = {
     self,
+    nixpkgs,
     darwin,
     home-manager,
     agenix,
+    flake-utils,
+    mac-app-util,
     ...
-  }: {
-    darwinConfigurations."LJQPCW4D95" = darwin.lib.darwinSystem {
-      modules = [
-        {
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
+  }:
+    {
+      darwinConfigurations."LJQPCW4D95" = darwin.lib.darwinSystem {
+        modules = [
+          {
+            # Set Git commit hash for darwin-version.
+            system.configurationRevision = self.rev or self.dirtyRev or null;
 
-          nix.settings.experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
+            nix.settings.experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
 
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        }
-        ./darwin
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.users.jessevanderpluijm = {...}: {
-            imports = [
-              ./home
-              agenix.homeManagerModules.default
+            nixpkgs = {
+              hostPlatform = "aarch64-darwin";
+              config = {
+                allowUnfree = true;
+              };
+            };
+          }
+          ./darwin
+          mac-app-util.darwinModules.default
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.jessevanderpluijm = {...}: {
+              imports = [
+                ./home
+                mac-app-util.homeManagerModules.default
+                agenix.homeManagerModules.default
+              ];
+            };
+          }
+        ];
+      };
+
+      editorHomeManagerConfiguration = home-manager.lib.homeManagerConfiguration {
+        pkgs = self.darwinConfigurations."LJQPCW4D95".pkgs;
+        modules = [
+          ./home
+          agenix.homeManagerModules.default
+        ];
+      };
+    }
+    // (
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nixd
+              alejandra
+              statix
+              deadnix
             ];
           };
         }
-      ];
-    };
-  };
+      )
+    );
 }
